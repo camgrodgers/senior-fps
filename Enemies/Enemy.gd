c0a1297264c93a24f85f3045d8bb84521ad20627
maiken1 <57634158+maiken1@users.noreply.github.com>
2021-01-08 11:53:40 -0500

    added "Patrol" state
    Patrol state if not fully functional yet. AI will just go the nearest node.

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -3,12 +3,17 @@ extends KinematicBody
 
 var nav: Navigation = null
 var target = null
+var navNodes: Array = []
 var path: Array = []
 var progress: float = 0
 
+var numNodes = 0
+var nodeIndex = 0
+
 enum {
 	FIND,
-	HOLD
+	HOLD,
+	PATROL
 }
 
 var state = FIND
@@ -21,6 +26,27 @@ func prep():
 	path = Array(path)
 	for p in path:
 		p.y = translation.y
+		
+func prep_node(node):
+	path = nav.get_simple_path(translation, node.translation, true)
+	path = Array(path)
+	for p in path:
+		p.y = translation.y
+
+func get_shortest_node():
+	var shortestNodePathDistance = INF
+	var shortestNodePathIndex = 0
+	var currentNodePathIndex = 0
+	for n in navNodes:
+		path = nav.get_simple_path(translation, n.translation, true)
+		var total_distance = 0
+		for point in path:
+				total_distance += translation.distance_to(point)
+		if total_distance < shortestNodePathDistance:
+			shortestNodePathIndex = currentNodePathIndex
+			shortestNodePathDistance = total_distance
+		currentNodePathIndex += 1
+	return prep_node(navNodes[shortestNodePathIndex])
 
 func _process(delta):
 #	path = nav.get_simple_path(translation, target.translation, true)
@@ -47,8 +73,9 @@ func _process(delta):
 			var total_distance = 0
 			for point in path:
 				total_distance += translation.distance_to(point)
-			if total_distance < 15:
+			if total_distance <= 15:
 				state = HOLD
+				path.clear()
 			if distance < moving:
 				path.pop_front()
 				return
@@ -56,15 +83,25 @@ func _process(delta):
 			translation = translation.linear_interpolate(to, moving / distance)
 		HOLD:
 			prep()
-			var speed = 4.0
-			var moving = speed * delta
 			var to = path[0]
 			var distance = translation.distance_to(to)
 			var total_distance = 0
 			for point in path:
 				total_distance += translation.distance_to(point)
 			if total_distance > 25:
-				state = FIND
+				state = PATROL
+				path.clear()
+		PATROL:
+			if path.size() < 1:
+				get_shortest_node()
+			var speed = 4.0
+			var moving = speed * delta
+			var to = path[0]
+			var distance = translation.distance_to(to)
+			if distance < moving:
+				path.pop_front()
+				return
+			translation = translation.linear_interpolate(to, moving / distance)
 	
 func take_damage():
 	self.queue_free()
