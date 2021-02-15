8f8c86e9e96fa91d98772714c5b95c820f7431d3
Cameron R <cameron.g.rodgers@gmail.com>
2021-02-15 11:56:40 -0500

    added level select screen

--- Main.gd
+++ Main.gd
@@ -2,8 +2,26 @@ extends Node
 
 var current_level: Navigation = null
 
-func _ready():
-	var current_level_scn: Resource = load("res://Levels/TestLevel.tscn")
+#func _ready():
+#	var current_level_scn: Resource = load("res://Levels/TestLevel.tscn")
+#	current_level = current_level_scn.instance()
+#	add_child(current_level)
+#	$Menu.visible = false
+
+
+func load_level(filename: String):
+	if current_level != null:
+		current_level.queue_free()
+	var current_level_scn: Resource = load(filename)
 	current_level = current_level_scn.instance()
 	add_child(current_level)
 	$Menu.visible = false
+
+func _on_Button3_pressed():
+	load_level("res://Levels/TestLevel.tscn")
+
+func _on_Button_pressed():
+	load_level("res://Levels/NewLevel/GameLevel.tscn")
+
+func _on_Button2_pressed():
+	load_level("res://Levels/CamLevel1/CamLevel1.tscn")
