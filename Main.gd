853c8baed11cb2c89f767dba1b78795905bdb71e
Cameron R <cameron.g.rodgers@gmail.com>
2021-02-15 11:58:45 -0500

    removed commented out code

--- Main.gd
+++ Main.gd
@@ -2,13 +2,6 @@ extends Node
 
 var current_level: Navigation = null
 
-#func _ready():
-#	var current_level_scn: Resource = load("res://Levels/TestLevel.tscn")
-#	current_level = current_level_scn.instance()
-#	add_child(current_level)
-#	$Menu.visible = false
-
-
 func load_level(filename: String):
 	if current_level != null:
 		current_level.queue_free()
