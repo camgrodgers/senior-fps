bef21aa5346055a7a592f34a7cfea9dc73d85c47
Cameron R <cameron.g.rodgers@gmail.com>
2021-03-20 12:47:49 -0400

    refactoring player danger calculation

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -316,3 +316,58 @@ func set_damage(d_mult: float):
 	current_damage_mult = d_mult
 func reset_damage():
 	current_damage_mult = DAMAGE_MULTIPLIER
+
+# Danger calculation
+var danger_decrease_acceleration: float = 6
+var danger_decrease_velocity: float = 0
+
+func danger_update(delta: float) -> void:
+	var rate = rate_of_danger_increase()
+	var player_position = player.translation
+	
+	player_danger = clamp(player_danger + (rate * delta), 0, 100)
+	if ((not world_state["can_see_player"])
+			and last_player_position.distance_to(player_position) < 6):
+		player_danger -= 3 * delta
+		danger_decrease_velocity = 0
+	elif not world_state["can_see_player"]:
+		player_danger -= (danger_decrease_velocity) * delta
+	else:
+		danger_decrease_velocity = 0
+	
+	danger_decrease_velocity += (danger_decrease_acceleration * delta)
+
+func rate_of_danger_increase() -> float:
+	var player_speed: float = player.vel.length()
+	
+	if not world_state["can_see_player"]:
+		return 0.0
+	
+	var rate: float = 0
+	if player_distance > 150:
+		rate = 0
+	elif player_distance > 100:
+		rate = 2
+	elif player_distance > 50:
+		rate = 10
+	elif player_distance > 30:
+		rate = 15
+	elif player_distance > 10:
+		rate = 20
+	else:
+		rate = 30
+	
+	rate = rate * current_damage_mult
+	
+	var speed_factor: float = 1
+	if player_speed < 2:
+		speed_factor = 2
+	elif player_speed > 15:
+		speed_factor = 0.75
+		#TODO: add a timer or something for the speed factor so it doesn't immediately go up when 
+		#you change directions
+		pass
+	
+	rate *= speed_factor
+	return rate
+
--- Player/PlayerStats.gd
+++ Player/PlayerStats.gd
@@ -2,9 +2,6 @@ extends Node
 
 var danger_level: float = 0
 var danger_level_orange: float = 0
-var danger_decrease_acceleration: float = 6
-# NOTE: This could be changed to a per-enemy value
-var danger_decrease_velocity: float = 0
 var known_cover_position: bool = false
 var hidden_from_enemies: bool = true
 
@@ -51,38 +48,3 @@ func danger_update(delta):
 	if danger_level == 0:
 		danger_decrease_velocity = 0
 
-func rate_of_danger_increase(enemy) -> float:
-	var player_distance: float = enemy.player_distance
-	var player_speed: float = get_parent().vel.abs().length()
-#	print(player_speed)
-	
-	if not enemy.world_state["can_see_player"]:
-		return 0.0
-	
-	var rate: float = 0
-	if player_distance > 150:
-		rate = 0
-	elif player_distance > 100:
-		rate = 2
-	elif player_distance > 50:
-		rate = 10
-	elif player_distance > 30:
-		rate = 15
-	elif player_distance > 10:
-		rate = 20
-	else:
-		rate = 30
-	
-	rate = rate * enemy.current_damage_mult
-	
-	var speed_factor: float = 1
-	if player_speed < 4:
-		speed_factor = 2
-	elif player_speed > 15:
-		speed_factor = 0.75
-		#add a timer or something for the speed factor so it doesn't immediately go up when 
-		#you change directions
-		pass
-	
-	rate *= speed_factor
-	return rate
